ecf15e0b3871a66375411ee26fb139a1cfc9a2b8
Hoàng Nhựt Duy <20130238@st.hcmuaf.edu.vn>
2023-01-03 16:27:27 +0700

    update admin

---
+++ src/main/java/Controller/DeleteCategory.java
@@ -0,0 +1,25 @@
+package Controller;
+
+import DAO.DAO;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "DeleteCategory", value = "/DeleteCategory")
+public class DeleteCategory extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String idCate = request.getParameter("id");
+        DAO dao = DAO.getInstance();
+        dao.deleteCategorybyID(idCate);
+        request.setAttribute("message","Xóa thành công");
+        request.getRequestDispatcher("AdminControl").forward(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
---
+++ src/main/java/Controller/DeleteUser.java
@@ -0,0 +1,26 @@
+package Controller;
+
+import DAO.DAO;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "DeleteUser", value = "/DeleteUser")
+public class DeleteUser extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String idString = request.getParameter("id");
+        int id = Integer.parseInt(idString);
+        DAO dao = DAO.getInstance();
+        dao.deleteUser(id);
+        request.setAttribute("message","Xóa thành công");
+        request.getRequestDispatcher("AdminControl").forward(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
