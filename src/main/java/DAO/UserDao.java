d9dec5531e325e2fc3f3d505160dd6d58873f5be
NguyenHoMinhCanh <20130209@st.hcmuaf.edu.vn>
2023-01-05 10:06:19 +0700

    message

---
+++ src/main/java/DAO/UserDao.java
@@ -0,0 +1,17 @@
+package DAO;
+
+import Model.User;
+
+import javax.persistence.EntityManager;
+
+public class UserDao {
+
+    public UserDao () {
+
+    }
+
+    public User findByUsername(String username){
+//        EntityManager em =
+        return new User();
+    }
+}
